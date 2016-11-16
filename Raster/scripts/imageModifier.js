70712f8b740dc4936b6342dcc6967d259154459b
madn1 <qserxsdiv@gmail.com>
2016-11-16 16:29:26 +0700

    Лаба 5 2

--- Raster/scripts/imageModifier.js
+++ Raster/scripts/imageModifier.js
@@ -1,11 +1,31 @@
 //https://habrahabr.ru/post/145816/
+"use strict";
 
-let imageData;
-let context = document.getElementById('ImageObject').getContext('2d');
+let imageObj;
+let context;
+let gistogrammData = [];
 
-function onLoadImage(value) {
-    context.drawImage(myImg, 0, 0);
-    imageData = context.getData();
+$(function () {
+    context = document.getElementById('ImagePreviewer').getContext('2d');
+
+    imageObj = new Image();
+
+    imageObj.onload = function () {
+        let canvas = document.getElementById('ImagePreviewer');
+        canvas.width = imageObj.width;
+        canvas.height = imageObj.height;
+        context.drawImage(imageObj, 0, 0);
+        analyseImage()
+    };
+
+    imageObj.src = 'originalImage.jpg';
+});
+
+function analyseImage() {
+    let data = context.getImageData(0, 0, imageObj.width, imageObj.height).data
+    foreach (function (elem) {
+        
+    })
 }
 
 function onChangeBrightness(value) {
